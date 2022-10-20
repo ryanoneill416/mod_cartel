5d75553a29dc9b7fd0f01ef5de68809336e9fe0a
Ryan O'Neill <ryanoneill416@gmail.com>
2022-10-20 20:14:23 +0000

    Create view for website homepage

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views import generic, View
+from .models import Build
 
-# Create your views here.
+
+class Home(View):
+    """
+    View for the home page
+    Will show the featured builds of the week
+    """
+
+    def get(self, request):
+        """
+        get request to retrieve the home page
+        with the featured posts included
+        """
+        featured_builds = Build.objects.filter(is_featured=True)[:4]
+        context = {
+            "featured_builds": featured_builds,
+        }
+        return render(request, 'index.html', context)
