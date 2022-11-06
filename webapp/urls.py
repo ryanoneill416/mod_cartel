255b5e46720d1ff4b8223d8ef111d416483b748a
Ryan O'Neill <ryanoneill416@gmail.com>
2022-11-06 19:07:36 +0000

    Implement edit_build template and url to allow users to edit posted builds

--- webapp/urls.py
+++ webapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('showcase/', views.Showcase.as_view(), name='showcase'),
     path('my_garage/', views.MyGarage.as_view(), name='my_garage'),
     path('add_build/', views.AddBuild.as_view(), name='add_build'),
+    path('edit_build/<int:pk>', views.EditBuild.as_view(), name='edit_build'),
 ]
