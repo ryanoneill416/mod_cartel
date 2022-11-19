0d6c8b01af74b7fc35cb8a969b3f244d3b70b11f
Ryan O'Neill <ryanoneill416@gmail.com>
2022-11-19 17:49:28 +0000

    Add build_save url to urls.py

--- webapp/urls.py
+++ webapp/urls.py
@@ -2,15 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Home.as_view(), name='home'),
-    path('showcase/', views.Showcase.as_view(), name='showcase'),
-    path('my_garage/', views.MyGarage.as_view(), name='my_garage'),
-    path('add_build/', views.AddBuild.as_view(), name='add_build'),
-    path('edit_build/<int:pk>', views.EditBuild.as_view(), name='edit_build'),
-    path('delete_build/<int:build_id>',
-         views.delete_build, name='delete_build'),
-    path('<slug:slug>/', views.BuildDetail.as_view(), name="build_detail"),
-    path('like/<slug:slug>/', views.BuildLike.as_view(), name='build_like'),
-    path('delete_comment/<int:comment_id>',
-         views.delete_comment, name='delete_comment'),
+     path('', views.Home.as_view(), name='home'),
+     path('showcase/', views.Showcase.as_view(), name='showcase'),
+     path('my_garage/', views.MyGarage.as_view(), name='my_garage'),
+     path('add_build/', views.AddBuild.as_view(), name='add_build'),
+     path('edit_build/<int:pk>/',
+          views.EditBuild.as_view(), name='edit_build'),
+     path('delete_build/<int:build_id>/',
+          views.delete_build, name='delete_build'),
+     path('<slug:slug>/', views.BuildDetail.as_view(), name="build_detail"),
+     path('delete_comment/<int:comment_id>',
+          views.delete_comment, name='delete_comment'),
+     path('like/<slug:slug>/', views.BuildLike.as_view(), name='build_like'),
+     path('save/<slug:slug>/', views.BuildSave.as_view(), name='build_save'),
 ]
